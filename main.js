0468a7819b382c996e43713a37fcc6007101b8c2
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2018-12-16 20:16:34 -0800

    Add size XS and account for window height

--- main.js
+++ main.js
@@ -62,21 +62,31 @@ if (localStorage.minesweeperBoard) {
 
 /* HANDLE WINDOW RESIZE */
 
-const BODY_MARGIN = 16;
-const MD_TILE_WIDTH = 25;
-const LG_TILE_WIDTH = 37;
-const XL_TILE_WIDTH = 49;
+const H_MARGIN = 16;
+const V_MARGIN = 44;
+const SM_TILE = 19;
+const MD_TILE = 25;
+const LG_TILE = 37;
+const XL_TILE = 49;
 
 function handleResize() {
   let windowWidth = window.innerWidth;
+  let windowHeight = window.innerHeight;
   let boardWidth = board.getWidth();
+  let boardHeight = board.getHeight();
   modifyDom(() => {
-    BOARD_EL.classList.remove('sm', 'md', 'lg', 'xl');
-    if (windowWidth <  MD_TILE_WIDTH * boardWidth + BODY_MARGIN) {
+    BOARD_EL.classList.remove('xs', 'sm', 'md', 'lg', 'xl');
+    if (windowWidth < SM_TILE * boardWidth + H_MARGIN
+        || windowHeight < SM_TILE * boardHeight + V_MARGIN) {
+      BOARD_EL.classList.add('xs');
+    } else if (windowWidth < MD_TILE * boardWidth + H_MARGIN
+        || windowHeight < MD_TILE * boardHeight + V_MARGIN) {
       BOARD_EL.classList.add('sm');
-    } else if (windowWidth <  LG_TILE_WIDTH * boardWidth + BODY_MARGIN) {
+    } else if (windowWidth < LG_TILE * boardWidth + H_MARGIN
+        || windowHeight < LG_TILE * boardHeight + V_MARGIN) {
       BOARD_EL.classList.add('md');
-    } else if (windowWidth <  XL_TILE_WIDTH * boardWidth + BODY_MARGIN) {
+    } else if (windowWidth < XL_TILE * boardWidth + H_MARGIN
+        || windowHeight < XL_TILE * boardHeight + V_MARGIN) {
       BOARD_EL.classList.add('lg');
     } else {
       BOARD_EL.classList.add('xl');
